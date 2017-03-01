68dbe497846a728afa4c4965787069a3998b59c8
GuardLDW <303899225@qq.com>
2017-03-01 22:45:36 +0800

    将项目分为引擎内核部分与测试部分，能够在测试部分运行上次作业的情景

---
+++ test/src/test.ts
@@ -0,0 +1,67 @@
+var canvas = document.getElementById("app") as HTMLCanvasElement;
+var stage = engine.run(canvas);
+
+engine.Ticker.getInstance().register((deltaTime) => {
+
+    console.log(deltaTime);
+    button.y = button.y + deltaTime / 10;
+
+});
+
+//第二层容器
+var panel = new engine.DisplayObjectContainer();
+panel.x = 120;
+panel.y = 50;
+panel.alpha = 0.5;
+
+//按钮
+var button = new engine.Button();
+button.x = 10;
+button.y = 30;
+button.text = "点我";
+button.color = "#FF0000"
+button.size = 20;
+button.enable = true;
+button.addEventListener("onclick", ()=>{
+
+    button.text = "欧尼酱";     
+
+},this,false);
+
+var word2 = new engine.TextField();
+word2.text = "第二层容器"
+word2.color = "#0000FF"
+word2.size = 30;
+
+var list = new engine.DisplayObjectContainer();
+list.addEventListener("onmousemove", (e : MouseEvent) =>{
+
+    var dy = engine.currentY - engine.lastY;
+    list.y = list.y + dy;
+
+}, this, false);
+
+
+//图片
+var avater = new engine.Bitmap();
+avater.image.src = "avater.jpg";
+
+
+//加载完图片资源
+avater.image.onload = () => {
+
+
+
+    list.addChild(avater);
+    list.addChild(button);
+        
+    panel.addChild(word2);
+
+    stage.addChild(list);
+    stage.addChild(panel);
+
+        
+}
+
+
+
